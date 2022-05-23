cfdb6e75a26c4e86868c0326840c768893b78105
blockfun <dotseven.kth@outlook.com>
2022-05-23 12:35:30 +0800

    add token description md

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
     介绍: ['doc','doc1', 'doc2', 'doc3', 'doc4', 'doc5'],
+    Token通证介绍: ['token'],
     NFT介绍: ['nft', 'nft1', 'nft2', 'nft3', 'nft4', 'nft5'],
     社区: ['dao1'],
     UGC申请: ['ugc1'],
