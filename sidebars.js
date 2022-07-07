3d25ff395687b30b785a54afafec4439f3c9e683
blockfun <dotseven.kth@outlook.com>
2022-07-07 17:16:29 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   someSidebar: {
     介绍: ['doc','doc1', 'doc2', 'doc3', 'doc4', 'doc5'],
     Token通证介绍: ['token'],
-    NFT介绍: ['NFT稀有度', 'NFT 八大种类', 'nft2', 'nft3', 'nft4', 'nft5'],
+    NFT介绍: ['NFT稀有度', 'NFT 八大种类', '类别和获取途径', 'nft3', 'nft4', 'nft5'],
     社区: ['dao1'],
     UGC申请: ['ugc1'],
     路线图: ['roadmap'],
