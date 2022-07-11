220e43116d9c00c26778fbf245fc240aad1f6640
blockfun <dotseven.kth@outlook.com>
2022-07-11 14:18:21 +0800

    updated

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   someSidebar: {
     启动阶段: ['项目理念', 'doc3', 'doc4', 'doc5'],
     简介: ['Muses-online','愿景','技术创新与破圈','Multiverse'],
-    'Getting started': ['How to'],
+    'Getting started': ['How to','玩法机制'],
     Tokenomics: ['doc1','Tokenomics'],
     NFTs: ['NFT稀有度', 'NFT 八大种类', '类别和获取途径', 'NFT standard', 'nft4', 'nft5'],
     Ecosystem: ['MUIP', '舞池大赛', 'Media kit', 'Community'],
