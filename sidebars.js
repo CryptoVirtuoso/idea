c8f49e3e504728d5b2f93f0cfe2b256ae30c988b
blockfun <dotseven.kth@outlook.com>
2022-07-11 13:26:50 +0800

    updated

--- sidebars.js
+++ sidebars.js
@@ -6,7 +6,7 @@ module.exports = {
     NFTs: ['NFT稀有度', 'NFT 八大种类', '类别和获取途径', 'NFT standard', 'nft4', 'nft5'],
     Ecosystem: ['MUIP', '舞池大赛', 'Media kit', 'Community'],
     'Developers': ['PGC'],
-    路线图: ['roadmap'],
+    路线图: ['roadmap', '赏金计划'],
     web3app: ['app1'],
   },
 };
