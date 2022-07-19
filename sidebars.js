e2c9c450f6758f569aa122a06b3fba50be664cd8
blockfun <92540911+cryptoascii@users.noreply.github.com>
2022-07-19 12:46:16 +0800

    updated

--- sidebars.js
+++ sidebars.js
@@ -1,20 +1,15 @@
-const sidebars = {
-  docs: [
-    'Muses-online',
-    {
-      type: 'category',
-      label: 'Getting Started',
-      link: {
-        type: 'generated-index',
-      },
-      collapsed: false,
-      items: [
-        'installation',
-        'configuration',
-        'playground',
-        'typescript-support',
-      ],
-    },
-  ],
+module.exports = {
+  someSidebar: {
+    启动阶段: ['项目理念', 'doc3', 'doc4', 'doc5'],
+    简介: ['Muses-online','愿景','技术创新与破圈','Multiverse'],
+    'Getting started': ['How to','玩法机制'],
+    Tokenomics: ['doc1','获取代币','代币标准协议及功能'],
+    NFTs: ['什么是NFT','类别和获取途径','属性及稀有度', '掉落预览','NFT 八大种类', 'NFT standard',  'NFT铸造方式及原理'],
+    Ecosystem: ['MUIP', '舞池大赛', 'Media kit', 'Community'],
+    'Developers': ['PGC', '赏金计划'],
+    路线图: ['roadmap'],
+    Faucet: ['MU加密水龙头', 'AuditStar映射规则'],
+    Privacy: ['Privacy'],
+    web3app: ['app1'],
+  },
 };
-module.exports = sidebars;
